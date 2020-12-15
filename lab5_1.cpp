b6239ca288de587a6464409c3cb89570e537de34
Nonthawat_K <Nonthawat_K@cmu.ac.th>
2020-12-15 15:37:36 +0700

    lab5_1.cpp completed

--- lab5_1.cpp
+++ lab5_1.cpp
@@ -2,8 +2,23 @@
 using namespace std;
 
 int main(){
-    cout << "Enter an integer: ";
-    cout << "#Even numbers = ";
-    cout << "#Odd numbers = ";
+   int i =0,Even=0,Odd=0,N;
+    while (N!=0)
+    {
+        cout << "Enter an integer: ";
+        cin>>N;
+        if (N%2==0)
+        {
+            Even++;
+        }
+        else 
+        {
+           Odd++;
+        }
+        
+       
+    }
+    cout << "#Even numbers = "<<Even-1<<endl;
+    cout << "#Odd numbers = "<<Odd;
     return 0;
 }
