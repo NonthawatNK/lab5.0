65ceb0a1286d3d864cbdaa481166d28b32a5a231
Nonthawat_K <Nonthawat_K@cmu.ac.th>
2020-12-15 16:21:58 +0700

    lab5_2.cpp completed

--- lab5_2.cpp
+++ lab5_2.cpp
@@ -1,7 +1,32 @@
+#define _USE_MATH_DEFINES
 #include <iostream>
-
+#include<cmath>
 using namespace std;
+double deg2rad(double a1){
+    double deg = a1*(M_PI/180);
+    return deg;
+}
+double rad2deg(double a2){
+   double rad = a2*(180/M_PI);
+    return rad;
+}
+double findXComponent(double l1,double l2,double a1,double a2){
+    double x = l1*cos(a1)+l2*cos(a2);
+    return x;
+}
+double findYComponent(double l1,double l2,double a1,double a2){
+   double y = l1*sin(a1)+l2*sin(a2);
+    return y;
+}
+double pythagoras(double xcomp,double ycomp){
+    double pytha = sqrt(pow(xcomp,2)+pow(ycomp,2));
+    return pytha;
 
+}
+void showResult(double result_vec_length,double result_vec_direction){
+    cout<<"Length of the resultant vector = "<<result_vec_length<<endl;
+    cout<<"Direction of the resultant vector (deg) = "<<result_vec_direction;
+}
 int main(){
     double l1,l2,a1,a2,xcomp,ycomp,result_vec_length,result_vec_direction;
     cout << "Enter length of the first vector: ";
