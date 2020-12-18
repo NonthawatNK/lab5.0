ca8d2ae74502a9154faad828f13ef6a71b40ea08
NonthawatNK <Nonthawt20@gmail.com>
2020-12-18 14:15:28 +0700

    exercise5_2 compeeed

---
+++ test/exercise5_2.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+using namespace std;
+int main(){
+    string text1 ;
+    string text2 ; 
+    int N ;
+    cout << "Enter the first text: ";
+    cin >> text1;
+    cout <<"Enter the second text: ";
+    cin >> text2;
+    cout << "Enter N: ";
+    cin >> N;
+    int count = 0;
+    while (count<=N)
+    {
+        if (count%2==0)
+        {
+            cout <<text1<<" ";  
+        }
+        else
+        {
+            cout << text2 <<" ";
+        }
+        count++;
+    }
+    
+
+
+}
